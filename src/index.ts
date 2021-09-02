18852c4aa32f29c60f367560cf34c329eb1274a8
NguyenDacChi <dacchi6761@gmail.com>
2021-09-02 22:39:37 +0700

    crud post

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,35 @@
+//import "reflect-metadata";
 import { MikroORM } from '@mikro-orm/core';
 import { __prod__ } from './constants';
 import mikroOrmConfig from './mikro-orm.config';
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+import { PostResolver } from './resolvers/post.resolver';
 
 const main = async () => {
   const orm = await MikroORM.init(mikroOrmConfig);
   await orm.getMigrator().up();
+
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [PostResolver],
+      validate: false,
+    }),
+    context: () => ({ em: orm.em }),
+  });
+  await apolloServer.start();
+  apolloServer.applyMiddleware({ app });
+
+  app.get('/', (_, res) => {
+    res.send('Hello World!');
+  })
+
+  app.listen(4000, () => {
+    console.log(`Server is running at port 4000!`);
+  });
 }
 
 main().catch(err => {
